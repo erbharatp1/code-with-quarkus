a794c88d107b9d1e9897b319f7b95bdf1b7d1e99
erbharatp1 <iambharat.p@gmail.com>
2026-03-07 13:50:11 +0530

    Add JSON logging and OpenAPI dependencies, add logging to Greeting endpoints

--- src/main/java/in/bharat/curd/GreetingController.java
+++ src/main/java/in/bharat/curd/GreetingController.java
@@ -1,5 +1,7 @@
 package in.bharat.curd;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,13 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/greeting")
 public class GreetingController {
 
+    private static final Logger log = LoggerFactory.getLogger(GreetingController.class);
+
     @GetMapping
     public String hello() {
+        log.info("Hello Spring ");
+        return "Hello Spring";
+    }
+
+    @GetMapping("/{name}")
+    public String hello(@PathVariable String name) {
+
         return "Hello Spring";
     }
 
     @PostMapping("/greet")
     public String sayHello(GreetingReq req) {
+        log.info("sayHello : ",req.greeting());
         return "Hello Spring: "+req.greeting();
     }
 }
--- src/main/java/in/bharat/curd/GreetingResource.java
+++ src/main/java/in/bharat/curd/GreetingResource.java
@@ -4,13 +4,20 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Path("/hello")
+
 public class GreetingResource {
+
+    private static final Logger log = LoggerFactory.getLogger(GreetingResource.class);
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        log.info("Hello from Quarkus REST");
+        log.info(GreetingResource.class.getSimpleName());
         return "Hello from Quarkus REST";
     }
 }
