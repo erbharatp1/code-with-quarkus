399f031fb6c2041878ba17fb60899d754ddf1d82
Bharat Patel <40136386+erbharatp1@users.noreply.github.com>
2026-06-08 15:58:59 +0530

    Add empty line in EmployeeRepository class
    Added a new line to the EmployeeRepository class.

--- src/main/java/in/bharat/curd/repository/EmployeeRepository.java
+++ src/main/java/in/bharat/curd/repository/EmployeeRepository.java
@@ -5,4 +5,6 @@ import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class EmployeeRepository implements PanacheRepository<Employee> {}
+public class EmployeeRepository implements PanacheRepository<Employee> {
+  
+}
